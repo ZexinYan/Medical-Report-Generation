871e10d1580707b792044495664a92e76c273d8a
Praj Bhattacharya <paraj.bhattacharjee@aot.edu.in>
2021-07-19 16:00:42 +0530

    changes removing cxr

--- utils/dataset.py
+++ utils/dataset.py
@@ -35,13 +35,14 @@ class ChestXrayDataSet(Dataset):
                 image_name = items[0]
                 label = items[1:]
                 label = [int(i) for i in label]
-                image_name = '{}.png'.format(image_name)
+                image_name = '{}.dcm.png'.format(image_name)
                 filename_list.append(image_name)
                 labels.append(label)
         return filename_list, labels
 
     def __getitem__(self, index):
         image_name = self.file_names[index]
+        image_name=image_name[3:]
         image = Image.open(os.path.join(self.image_dir, image_name)).convert('RGB')
         label = self.labels[index]
         if self.transform is not None:
